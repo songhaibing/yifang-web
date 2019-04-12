c72a3bc23adfad24437c7f9b522f9e80c4ea317d
shb <2269629396@qq.com>
2019-04-12 17:45:19 +0800

    2323

--- h5-web/src/view/main/academic-center/call-name/call-name.vue
+++ h5-web/src/view/main/academic-center/call-name/call-name.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>点名</div>
+  <div>
+    <main-header :header-name="headerName" :show="isShow" style="background-color: rgba(241, 241, 241, 1)"></main-header>
+  </div>
 </template>
 
 <script>
+  import MainHeader from "../../../../components/header/header";
   export default {
-    name: "call-name"
+    name: "call-name",
+    components: {MainHeader},
+    data(){
+      return{
+        headerName:'点名',
+        isShow:true
+      }
+    }
   };
 </script>
 
